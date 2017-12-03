c1203303687ad536a75e0e5e20b41d087e985986
Patriot720 <8457664+Patriot720@users.noreply.github.com>
2017-12-03 12:06:50 +0300

    added GCMTask

---
+++ src/main/java/example/cerki/osuhub/MyTaskService.java
@@ -0,0 +1,18 @@
+package example.cerki.osuhub;
+
+import android.app.Service;
+
+import com.google.android.gms.gcm.GcmNetworkManager;
+import com.google.android.gms.gcm.GcmTaskService;
+import com.google.android.gms.gcm.TaskParams;
+
+/**
+ * Created by cerki on 03-Dec-17.
+ */
+
+public class MyTaskService extends GcmTaskService {
+    @Override
+    public int onRunTask(TaskParams taskParams) {
+        
+    }
+}
