ee0b770fc3be74ec562e7d2db4400fcbb27b8026
Andrew <cerkin-3@yandex.ru>
2018-01-03 13:33:35 +0300

    changing ScoreboardFrgment to a simple view

--- src/main/java/example/cerki/osuhub/ui/ViewWrappers/ScoreboardViewWrap.java
+++ src/main/java/example/cerki/osuhub/ui/ViewWrappers/ScoreboardViewWrap.java
@@ -1,16 +1,39 @@
 package example.cerki.osuhub.ui.ViewWrappers;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import example.cerki.osuhub.R;
 
 /**
  * Created by cerki on 03.01.2018.
  */
 
 public class ScoreboardViewWrap {
-    FrameLayout scoreboardFrame;
+    @BindView(R.id.recycler)
+    RecyclerView mRecycler;
+    @BindView(R.id.progress_bar)
+    ProgressBar mProgressBar;
 
     public ScoreboardViewWrap(FrameLayout scoreboardFrame) {
-        this.scoreboardFrame = scoreboardFrame;
+        ButterKnife.bind(scoreboardFrame);
     }
 
+    public boolean isUpdating(){
+        return mRecycler.getVisibility() == View.GONE;
+    }
+    public void setUpdating(boolean bool){
+        if(bool){
+            mRecycler.setVisibility(View.GONE);
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+        else{
+            mRecycler.setVisibility(View.VISIBLE);
+            mProgressBar.setVisibility(View.GONE);
+        }
+    }
 }
