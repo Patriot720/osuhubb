48aec73d50527549bb2afbfa3294f8a1fda5e1b7
Andrew <cerkin-3@yandex.ru>
2018-01-04 12:41:18 +0300

    added ProgressBar,removed GenericRecyclerFragment,FeedRecyclerFragment

--- src/main/java/example/cerki/osuhub/ui/Fragments/FeedItemFragment.java
+++ src/main/java/example/cerki/osuhub/ui/Fragments/FeedItemFragment.java
@@ -1,36 +1,67 @@
 package example.cerki.osuhub.ui.Fragments;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import eu.davidea.flexibleadapter.FlexibleAdapter;
+import eu.davidea.flexibleadapter.items.IFlexible;
+import example.cerki.osuhub.Data.POJO.FeedItem;
 import example.cerki.osuhub.Logic.Tasks.FeedDbTask;
 import example.cerki.osuhub.Logic.Tasks.FeedNetworkTask;
 import example.cerki.osuhub.R;
+import example.cerki.osuhub.ui.Activities.BeatmapActivity;
+import example.cerki.osuhub.ui.FlexibleAdapterExtension;
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 
 @SuppressWarnings("unchecked")
-public class FeedItemFragment extends FeedRecyclerFragment{
-    @Override
-    public int getLayoutResource() {
-        return R.layout.fragment_feeditem_list;
+public class FeedItemFragment extends Fragment
+implements FlexibleAdapterExtension.OnDataUpdatedListener ,
+        FlexibleAdapter.OnItemClickListener{
+    @BindView(R.id.list)
+    RecyclerView recyclerView;
+    @BindView(R.id.refresh)
+    SwipeRefreshLayout refreshLayout;
+    private FlexibleAdapterExtension adapter;
+
+    public FeedItemFragment() {
     }
 
-
+    @Nullable
     @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_player_list, container, false); // Todo same view as player_list
+        ButterKnife.bind(view);
+        adapter = new FlexibleAdapterExtension<>(null,this);
+        recyclerView.setAdapter(adapter);
+        refreshLayout.setOnRefreshListener(this::updateData);
+        updateData();
+        initDataDatabase();
+        return view;
+    }
+
     public void initDataDatabase() {
-        new FeedDbTask(items -> {
-            this.onUpdate(items);
-            updateData();
-        }).execute();
+        new FeedDbTask(items -> adapter.updateDataSet(items)).execute();
     }
 
-    @Override
     public void updateData() {
-        setUpdating(true);
+        refreshLayout.setRefreshing(true);
         Single.fromCallable(FeedNetworkTask::getFeedItems) // Todo refactor
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(this::addItemsOnTop);
+                .subscribe(items->adapter.addItems(0,items));
     }
 
     public static FeedItemFragment newInstance(){
@@ -38,7 +69,20 @@ public class FeedItemFragment extends FeedRecyclerFragment{
     }
 
     @Override
-    public void onLoadMore(int lastPosition, int currentPage) {
+    public boolean onItemClick(int position) {
+        IFlexible item = adapter.getItem(position);
+        if(item instanceof FeedItem) {
+            FeedItem feedItem = (FeedItem) item;
+            Intent intent = new Intent(getContext(), BeatmapActivity.class);
+            intent.putExtra("beatmapId",feedItem.beatmap_id);
+            intent.putExtra("coverUrl",feedItem.coverUrl);
+            startActivity(intent);
+        }
+        return true;
+    }
 
+    @Override
+    public void onDataUpdated() {
+        refreshLayout.setRefreshing(false);
     }
 }
--- src/main/java/example/cerki/osuhub/ui/Fragments/ListFragment.java
+++ src/main/java/example/cerki/osuhub/ui/Fragments/ListFragment.java
@@ -1,22 +1,41 @@
 package example.cerki.osuhub.ui.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import eu.davidea.flexibleadapter.FlexibleAdapter;
+import eu.davidea.flexibleadapter.items.IFlexible;
 import example.cerki.osuhub.Data.ApiDatabase.ApiDatabase;
 import example.cerki.osuhub.Data.POJO.User;
 import example.cerki.osuhub.Logic.Tasks.ListTask;
 import example.cerki.osuhub.R;
+import example.cerki.osuhub.ui.FlexibleAdapterExtension;
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 
 @SuppressWarnings("unchecked")
-public class ListFragment extends GenericRecyclerFragment<User>{
-    @Override
-    protected int getEndlessScrollPageSize() {
-        return 0;
-    }
+public class ListFragment extends Fragment
+implements FlexibleAdapterExtension.OnDataUpdatedListener ,
+        FlexibleAdapter.OnItemClickListener,
+        FlexibleAdapter.EndlessScrollListener{
+    @BindView(R.id.list)
+    RecyclerView recyclerView;
+    @BindView(R.id.refresh)
+    SwipeRefreshLayout refreshLayout;
+    private FlexibleAdapterExtension adapter;
 
-    @Override
+
     protected void onItemClick(User item) {
         android.support.v4.app.Fragment fragment = PlayerFragment.newInstance(item.getUserId(), item.getUsername());
         getFragmentManager().beginTransaction().add(R.id.content_main, fragment)
@@ -24,25 +43,29 @@ public class ListFragment extends GenericRecyclerFragment<User>{
                 .commit();
     }
 
+    @Nullable
     @Override
-    public int getLayoutResource() {
-        return R.layout.fragment_player_list;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_player_list, container, false);
+        ButterKnife.bind(view);
+        adapter = new FlexibleAdapterExtension<>(null,this);
+        recyclerView.setAdapter(adapter);
+        refreshLayout.setOnRefreshListener(this::updateData);
+        refreshLayout.setRefreshing(true);
+        updateData();
+        initDataDatabase();
+        return view;
     }
 
-    @Override
     public void initDataDatabase() {
         Single.fromCallable(()-> ApiDatabase.getInstance().userDao().getAll())
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe((items)->{
-                    onUpdate(items);
-                    updateData();
-                });
+                .subscribe((items)-> adapter.updateDataSet(items));
     }
 
-    @Override
     public void updateData() {
-        new ListTask(this::onUpdate).loadUsers();
+        new ListTask(adapter::onLoadMoreComplete).loadUsers();
     }
 
     public static ListFragment newInstance() {
@@ -50,7 +73,23 @@ public class ListFragment extends GenericRecyclerFragment<User>{
     }
 
     @Override
+    public void noMoreLoad(int newItemsSize) {}
+
     public void onLoadMore(int lastPosition, int currentPage) {
-        new ListTask(getAdapter()::onLoadMoreComplete).loadUsersFromPage(currentPage+1);
+        new ListTask((items)->adapter.onLoadMoreComplete(items)).loadUsersFromPage(currentPage+1);
     }
+
+    @Override
+    public boolean onItemClick(int position) {
+        IFlexible item = adapter.getItem(position);
+        if(item instanceof User)
+            onItemClick((User) item);
+        return true;
+    }
+
+    @Override
+    public void onDataUpdated() {
+    refreshLayout.setRefreshing(false);
+    }
+
 }
